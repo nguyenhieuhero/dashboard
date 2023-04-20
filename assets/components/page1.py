c5e7709a03ee44be850b4c18c7ea7ece5bd6bcc9
nguyencong1227 <nguyenchiconganh12@gmail.com>
2023-04-20 11:40:51 +0700

    submit page 1

--- assets/components/page1.py
+++ assets/components/page1.py
@@ -1,6 +1,27 @@
+from cv2 import applyColorMap
 from dash import html
-page1=html.Div(className="app-page1",
-               children=[
-                html.H1("Page 1",className="p1-label")
+import base64
+region = ['LC', 'a', 'c']
+iVN = base64.b64encode(open("assets\image\VN.png", 'rb').read())
+iLC = base64.b64encode(open("assets\image\LC.png", 'rb').read())
+iSME = base64.b64encode(open("assets\image\SME.png", 'rb').read())
+iSSE = base64.b64encode(open("assets\image\SSE.png", 'rb').read())
 
-               ])
+page1=html.Div(className=".app-page1",
+               children=[
+                    html.H3("Data Overview",className="h1", style= {"margin-bottom": "0px", 'color' : 'black'}),
+                    html.H5('Tháng 4', style = {"margin-top": "0px", 'color' : 'black'}),
+                    
+                    html.H4("HUB", className="h4"),
+                    html.Img(src='data:image/png;base64,{}'.format(iVN.decode()),style={'width':"90vh"}),
+                    
+                    html.H4("Phân khúc", className = "h4"),
+                    html.Div(className = "three", 
+                             children = [
+                                 html.Img(src='data:image/png;base64,{}'.format(iLC.decode()),style={'width':"50vh"}),
+                                 html.Img(src='data:image/png;base64,{}'.format(iSME.decode()),style={'width':"50vh"}),
+                                 html.Img(src='data:image/png;base64,{}'.format(iSSE.decode()),style={'width':"50vh"}),
+                             ])       
+
+                ]
+               )
